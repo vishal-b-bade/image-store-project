70e7172a38a331d6acba8cb6647cf256eb36689c
Vishal-Bade <vishalbade1997@gmail.com>
2024-06-14 15:11:52 +0530

    config file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+    '/api': {
+      // target: 'http://localhost:3000/',
+      target: 'http://localhost:5173/',
+      changeOrigin: true,
+      secure: false,
+      rewrite: (path) => path.replace(/^\/api/, '')
+    },
+    cors:false
+    },
+  },
 })
